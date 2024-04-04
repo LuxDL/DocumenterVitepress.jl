910be4fc5ebecd25aaf06382dbbe1a315889b68d
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-04-04 13:38:11 -0400

    Reset index.ts to template state

--- docs/src/.vitepress/theme/index.ts
+++ docs/src/.vitepress/theme/index.ts
@@ -1,22 +1,13 @@
 // .vitepress/theme/index.ts
-import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import AsideTrustees from '../../components/AsideTrustees.vue'
 
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import './style.css'
 
 export default {
   extends: DefaultTheme,
-  Layout() {
-    return h(DefaultTheme.Layout, null, {
-      'aside-ads-before': () => h(AsideTrustees),
-    })
-  },
   enhanceApp({ app }) {
     enhanceAppWithTabs(app)
-    // register your custom global components
-    app.component('aside-trustees', AsideTrustees)
   }
 } satisfies Theme
