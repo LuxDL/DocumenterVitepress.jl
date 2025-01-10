e3b401e971e4768602497d0425067c90320d103d
Lazaro Alonso <lazarus.alon@gmail.com>
2025-01-10 19:09:21 +0100

    add to template

--- template/src/.vitepress/theme/index.ts
+++ template/src/.vitepress/theme/index.ts
@@ -3,6 +3,8 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import VersionPicker from "../../components/VersionPicker.vue"
+import AuthorBadge from '../../components/AuthorBadge.vue'
+import Authors from '../../components/Authors.vue'
 
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import './style.css'
@@ -17,5 +19,7 @@ export default {
   enhanceApp({ app, router, siteData }) {
     enhanceAppWithTabs(app);
     app.component('VersionPicker', VersionPicker);
+    app.component('AuthorBadge', AuthorBadge)
+    app.component('Authors', Authors)
   }
 } satisfies Theme
