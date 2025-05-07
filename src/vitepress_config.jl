e3fed1f69d6ef2a57c92bf6e50c0809db71a53df
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2025-05-07 10:05:12 +0200

    don't add repo abspath in local builds so docs don't break with LiveServer

--- src/vitepress_config.jl
+++ src/vitepress_config.jl
@@ -78,8 +78,11 @@ function modify_config_file(doc, settings, deploy_decision, i_folder, base)
     # folder = deploy_decision.subfolder
 
     deploy_relpath = "$(base)$(isempty(base) ? "" : "/")"
-    deploy_abspath = if isnothing(settings.deploy_url)
-        "/" * splitpath(settings.repo)[end]  # Get the last identifier of the repo path, i.e., `user/$repo`.
+    deploy_abspath = if isempty(base) && !haskey(ENV, "CI")
+            @info "Base is \"\" and ENV[\"CI\"] is not set so this is a local build. Not adding any additional base prefix based on the repository or deploy url and instead using absolute path \"/\" to facilitate serving docs locally."
+            "/"
+        elseif isnothing(settings.deploy_url)
+            "/" * splitpath(settings.repo)[end]  # Get the last identifier of the repo path, i.e., `user/$repo`.
         else
             s_path = startswith(settings.deploy_url, r"http[s?]:\/\/") ? splitpath(settings.deploy_url)[2:end] : splitpath(settings.deploy_url)
             s = length(s_path) > 1 ? joinpath(s_path) : "" # ignore custom URL here
